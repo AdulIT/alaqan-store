73c1ed974d87120eaba98a299ffea83dbf095022
AdulIT <spilberg.max@bk.ru>
2023-12-13 16:25:13 +0600

    refactor: translate productMain section

--- src/components/ProductMain/ProductMain.jsx
+++ src/components/ProductMain/ProductMain.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom';
+import { useTranslation } from 'react-i18next'
 
 import { useMatchMedia } from '../../hooks/useMatchMedia'
 
@@ -10,37 +11,38 @@ import fbl700 from '../../assets/img/products/turnstile-fbl700.svg'
 import terminalT1 from '../../assets/img/products/alaqan-terminalT1.svg'
 
 const ProductMain = () => {
+    const { t } = useTranslation(["products"])
     const location = useLocation()
 
     const {isMobile, isSmallMobile, isMicroMobile} = useMatchMedia();
 
     const turnstileNames = {
         '/product/1': {
-            text: isSmallMobile ? `Турникет-трипод Alaqan \n TS2000 Pro классика \n для офисных зданий`
-            : `Турникет-трипод Alaqan TS2000 Pro \n классика для офисных зданий`,
-            title: isSmallMobile ? 'Безопасная \n классика' : 'Безопасная классика',
+            text: isSmallMobile ? t(`all.${0}.mainTextSmallMobile`)
+                : t(`all.${0}.mainText`),
+            title: isSmallMobile ? t(`all.${0}.mainTitleSmallMobile`) : t(`all.${0}.mainTitle`),
             img: ts2000,
         },
         '/product/2': {
-            text: isMicroMobile ? 'Турникет-трипод Alaqan\n TS1000M Pro экономичный \n вариант для офисных зданий'
-            : isSmallMobile ? 'Турникет-трипод Alaqan \n TS1000M Pro экономичный вариант \n для офисных зданий'
-            : 'Турникет-трипод Alaqan TS1000M Pro \n экономичный вариант для офисных зданий',
-            title: isMicroMobile ? 'Выгодно \n и\n практично'
-            : isSmallMobile ? 'Выгодно \n и практично' : 'Выгодно и практично',
+            text: isMicroMobile ? t(`all.${1}.mainTextMicroMobile`)
+                : isSmallMobile ? t(`all.${1}.mainTextSmallMobile`)
+                : t(`all.${1}.mainText`),
+            title: isMicroMobile ? t(`all.${1}.mainTitleMicroMobile`)
+                 : isSmallMobile ? t(`all.${1}.mainTitleSmallMobile`) : t(`all.${1}.mainTitle`),
             img: ts1000,
         },
         '/product/3': {
-            text: isMicroMobile ? 'Автоматизированный \n турникет\nAlaqan FBL700 идеален \nдля офисных зданий \nс большим количеством \nсотрудников'
-            : isSmallMobile ? 'Автоматизированный турникет\nAlaqan FBL700 идеален \nдля офисных зданий \nс большим количеством \nсотрудников'
-            : 'Автоматизированный турникет Alaqan FBL700 \n идеален для офисных зданий \n с большим количеством сотрудников',
-            title: isMicroMobile ? 'Комфортно \nи\n эффективно'
-            : isMobile ? 'Комфортно \nи эффективно'  : 'Комфортно и эффективно',
+            text: isMicroMobile ? t(`all.${2}.mainTextMicroMobile`)
+                : isSmallMobile ? t(`all.${2}.mainTextSmallMobile`)
+                : t(`all.${2}.mainText`),
+            title: isMicroMobile ? t(`all.${2}.mainTitleMicroMobile`)
+                : isMobile ? t(`all.${2}.mainTitleMobile`) : t(`all.${2}.mainTitle`),
             img: fbl700,
         },
         '/product/10': {
-            text: isMicroMobile ? 'Настенный терминал \n учета рабочего времени \nс системой Alaqan. \n Пожалуй лучшее решение\n для оптимизации\n рабочего процесса.'
-            : 'Настенный терминал учета рабочего \n времени с системой Alaqan. \n Пожалуй лучшее решение для \n оптимизации рабочего процесса.',
-            title: isSmallMobile ? 'Быстро\n и практично' : 'Быстро и практично',
+            text: isMicroMobile ? t(`all.${9}.mainTextMicroMobile`)
+            : t(`all.${9}.mainText`),
+            title: isSmallMobile ? t(`all.${9}.mainTitleSmallMobile`) : t(`all.${9}.mainTitle`),
             img: terminalT1,
         },
     }
