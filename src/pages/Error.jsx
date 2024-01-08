8e83c79cee5a491a5c51adc0bf6942d7df4e345b
AdulIT <spilberg.max@bk.ru>
2024-01-08 17:23:18 +0600

    feat: error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { useScrollHeader } from '../hooks/useScrollHeader';
+
 import errorImg from '../assets/img/404/404.jpg'
 
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
 const Error = () => {
+    const {headerClosedStyles} = useScrollHeader()
+    const { t } = useTranslation(["errorpage"])
+
     return (
-        <div>
-            <img
-                src={errorImg}
-                alt="error"
-                loading='lazy'
-            />
-            <p>Страница не найдена :(</p>
-        </div>
+        <>
+            <Header style={headerClosedStyles} />
+            <main className='errorpage_main'>
+                <img
+                    src={errorImg}
+                    alt="error"
+                    loading='lazy'
+                />
+                <p> {t("errorText")} </p>
+            </main>
+            <Footer />
+        </>
     );
 };
 
