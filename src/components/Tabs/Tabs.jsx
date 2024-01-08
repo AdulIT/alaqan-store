34af38ddec511429f03e19870c8574f20b055718
AdulIT <spilberg.max@bk.ru>
2024-01-08 16:34:26 +0600

    feat: change image on lang change

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -6,8 +6,12 @@ import Button from '../UI/button/Button'
 import cl from './Tabs.module.css'
 
 import historyImg from '../../assets/img/hrApp/history.jpg'
+import historyImgEN from '../../assets/img/hrApp/history-en.jpg'
 import usersImg from '../../assets/img/hrApp/users.jpg'
+import usersImgEN from '../../assets/img/hrApp/users-en.jpg'
 import schedulesImg from '../../assets/img/hrApp/schedules.jpg'
+import schedulesImgEN from '../../assets/img/hrApp/schedules-en.jpg'
+
 import tripleStickIcon from '../../assets/icons/tripleSticks.svg'
 import curlyArrowIcon from '../../assets/icons/curlyArrow.svg'
 
@@ -39,25 +43,43 @@ const Tabs = () => {
         setToggleState(index)
     }
 
+    const images = [
+        {
+            en: historyImgEN,
+            ru: historyImg,
+            kz: historyImg,
+        },
+        {
+            en: usersImgEN,
+            ru: usersImg,
+            kz: usersImg,
+        },
+        {
+            en: schedulesImgEN,
+            ru: schedulesImg,
+            kz: schedulesImg,
+        },
+    ]
+
     return (
         <div className={cl.tabs__container}>
             <img className={cl.tabs_decorative_up} src={tripleStickIcon} alt="triple-stick" loading='lazy' />
             <div className={cl.tabs__content}>
                 <img
                     className={toggleState === 1 ? cl.tabs__content_img + ' ' + cl.show : cl.tabs__content_img + ' ' + cl.hide}
-                    src={historyImg}
+                    src={images[0][localStorage.getItem('i18nextLng')]}
                     alt="history-page"
                     loading='lazy'
                 />
                 <img
                     className={toggleState === 2 ? cl.tabs__content_img + ' ' + cl.show : cl.tabs__content_img + ' ' + cl.hide}
-                    src={usersImg}
+                    src={images[1][localStorage.getItem('i18nextLng')]}
                     alt="users-page"
                     loading='lazy'
                 />
                 <img
                     className={toggleState === 3 ? cl.tabs__content_img + ' ' + cl.show : cl.tabs__content_img + ' ' + cl.hide}
-                    src={schedulesImg}
+                    src={images[2][localStorage.getItem('i18nextLng')]}
                     alt="schedules-page"
                     loading='lazy'
                 />
